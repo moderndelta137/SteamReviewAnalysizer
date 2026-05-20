67a62d45182c8c2fd6b5ba893c279e46bf57429f
Dun Mao <moderndelta137@gmail.com>
2026-05-20 13:53:45 +0900

    Fix bat file

--- run-local-dev.bat
+++ run-local-dev.bat
@@ -11,12 +11,14 @@ if not exist "node_modules" (
 )
 
 set "DEV_URL=http://127.0.0.1:8787"
+set "XDG_CONFIG_HOME=%~dp0.wrangler-config"
+set "WRANGLER_LOG=none"
 
 echo starting local dev server...
-start "" "%ComSpec%" /k "cd /d ""%~dp0"" && npm run dev"
+start "" "%ComSpec%" /k "cd /d ""%~dp0"" && npm.cmd run dev -- --port 8787"
 
 echo opening %DEV_URL%
-timeout /t 3 /nobreak >nul
+ping 127.0.0.1 -n 4 >nul
 start "" "%DEV_URL%"
 
 endlocal
